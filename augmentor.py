8be4e1cdff10e950c7b18e9309c200ce9d3272a9
duo <43629177+laowu2011@users.noreply.github.com>
2023-08-03 11:12:50 +0800

    Update augmentor.py

--- augmentor.py
+++ augmentor.py
@@ -1,83 +1,65 @@
 import os
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import numpy as np
-from torch.utils.data import DataLoader, TensorDataset
+import sys
+sys.path.append("..//")
+from model.HTVAE import HTVAE
+from model.HTVAE import tokenize_input
+from model.HTVAE import build_vocab
+from model.HTVAE import tokens_to_indices
 
-# Define Encoder Model (same as before)
+# ... [Only keep the required functions and class definitions]
 
-# Define Decoder Model (same as before)
+def generate_samples(vae, num_samples, seq_length, vocab):
+    vae.eval()  # Set the VAE to evaluation mode
+    reverse_vocab = {v: k for k, v in vocab.items()}  # Create a reverse vocabulary
 
-# Define HT-VAE Model (same as before)
+    generated_samples = []
 
-# Load the syntax vectors (Assuming you have generated and saved them using paser.py)
-syntax_vectors = []
-for i in range(600):
-    with open(f"{i}_syntax_vector.txt", "r") as f:
-        syntax_vector = f.read().splitlines()
-    syntax_vectors.append(syntax_vector)
+    for _ in range(num_samples):
+        z = torch.randn((1, seq_length, vae.mean_layer.out_features)).to(device)
+        with torch.no_grad():
+            dec_output, _ = vae.decoder_rnn(z)
+            x_hat = vae.decoder_output(dec_output)
 
-# Preprocess the syntax vectors (convert to numpy array and pad if necessary)
-max_seq_length = max(len(seq) for seq in syntax_vectors)
-padded_syntax_vectors = [seq + ['PAD'] * (max_seq_length - len(seq)) for seq in syntax_vectors]
-padded_syntax_vectors = np.array(padded_syntax_vectors)
+        # Get the most probable tokens
+        generated_seq = torch.argmax(x_hat.squeeze(), dim=-1).cpu().numpy()
 
-# Create PyTorch DataLoader
-batch_size = 32
-syntax_tensor = torch.tensor(padded_syntax_vectors, dtype=torch.float32)
-dataset = TensorDataset(syntax_tensor)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+        # Convert indices back to tokens
+        generated_tokens = [reverse_vocab[idx] for idx in generated_seq if idx != 0]  # Ignore the PAD token
+        generated_samples.append(" ".join(generated_tokens))
 
-# Define model hyperparameters (same as before)
+    return generated_samples
 
-# Build and train the HT-VAE model (same as before)
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # Build vocabulary from all files in the original data directory
+    data_dir = '../data/AES-T_Sequence'
+    all_tokens = []
+    for filename in os.listdir(data_dir):
+        if filename.endswith(".txt"):
+            with open(os.path.join(data_dir, filename), 'r', encoding='utf-8') as f:
+                content = f.read()
+                tokens = tokenize_input(content)
+                all_tokens.extend(tokens)
 
-# Load the trained model
-htvae = HT_VAE(input_shape, latent_dim)
-htvae.load_state_dict(torch.load("trained_htvae_model.pt"))
-htvae.eval()
+    vocab = build_vocab(all_tokens)
 
-# Generate samples using the trained decoder
-def generate_samples(htvae, num_samples, max_seq_length, latent_dim):
-    samples = []
+    # Initialize the VAE model
+    vae = HTVAE(len(vocab))
 
-    with torch.no_grad():
-        for _ in range(num_samples):
-            # Initialize the input with Gaussian distribution and SOS identifier
-            input_latent = torch.randn(1, latent_dim)
-            sos_token = torch.tensor([[1] + [0] * (max_seq_length - 1)], dtype=torch.float32)
-            sos_token = sos_token.unsqueeze(1)  # Add batch dimension
+    # Load the trained model
+    vae.load_state_dict(torch.load('../model/trained_ht_vae.pt'))
+    vae = vae.to(device)
 
-            # Generate sequence using the trained decoder
-            generated_sequence = []
-            decoder_input = torch.cat((input_latent, sos_token), dim=2)  # Concatenate latent input and SOS identifier
-
-            for step in range(max_seq_length):
-                decoder_output = htvae.decoder(decoder_input)
-                generated_token = torch.argmax(decoder_output[0, 0, :])  # Get the most probable token index
-
-                if generated_token == 0:  # EOS token
-                    break
-
-                generated_sequence.append(generated_token.item())
-                decoder_input = torch.cat((input_latent, decoder_output[:, :, :step+2]), dim=2)  # Update decoder input
-
-            samples.append(generated_sequence)
-
-    return samples
-
-# Generate 1000 samples
-num_samples = 1000
-generated_samples = generate_samples(htvae, num_samples, max_seq_length, latent_dim)
-
-# Save the generated samples in the 'sequence' folder
-if not os.path.exists("sequence"):
-    os.mkdir("sequence")
-
-for i, sample in enumerate(generated_samples):
-    filename = os.path.join("sequence", f"{i}.txt")
-    with open(filename, "w") as f:
-        f.write(" ".join(str(token) for token in sample))
-
-print("Generated samples saved in the 'sequence' folder.")
+    # Generate samples
+    generated_samples = generate_samples(vae, 1000, 354, vocab)
+    
+    # Save generated samples
+    save_dir = '../data/AES-T_Aug_Sequence'
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    
+    for i, sample in enumerate(generated_samples):
+        with open(os.path.join(save_dir, f'generated_sample_{i}.txt'), 'w', encoding='utf-8') as f:
+            f.write(sample)
